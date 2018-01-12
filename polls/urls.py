ec4fcea55bc3532b344ac917b8f8da717b1bae66
HosanaUFRRJ2014 <hosannagomes@gmail.com>
2018-01-12 12:55:15 -0200

    tutorial - part 3 - adding views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
+                #/polls/
                 path('', views.index, name='index'),
-                #path('', beginning_page.html, name='html'),
+                #/polls/<question_id>/
+                path('<int:question_id>/', views.detail, name='detail'),
+                #/polls/<question_id>/results/
+                path('<int:question_id>/results/', views.results, name='results'),
+                #/polls/<question_id>/vote/
+                path('<int:question_id>/vote/', views.vote, name='vote'),
               ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("I love Doctor Who.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+
+    context = {
+                'latest_question_list': latest_question_list,
+    }
+
+    #response = "List of latest questions added:<br><br>"
+    #response += '<br>'.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(template.render(context,request))
+    return render(request,'polls/index.html', context)
+
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+
+    #Longest way to do it:
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    ##    response = "You are looking at question %s.<br>" + question_text
+    #except Question.DoesNotExist:
+    #    raise Http404("Question does not exist.")
+    ##    response = "Question number %s does not exist."
+
+
+
+def results(request,question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
