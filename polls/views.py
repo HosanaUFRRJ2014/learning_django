75f89caed15d01a115587f5638bc466b1e2605c2
HosanaUFRRJ2014 <hosannagomes@gmail.com>
2018-01-16 11:57:16 -0200

    tutorial - part 4 - Adding simple form and generic views

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,49 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-                'latest_question_list': latest_question_list,
-    }
-    return render(request,'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions """
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question' : question})
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
 
-
-def results(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
-
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/vote.html', {'question': question})
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        context = {
+                    'question': question,
+                    'error_message': "You didn't select a choice.",
+
+        }
+        return render(request, 'polls/detail.html',context)
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        #Always return an HttpResponseRedirect after successfully dealing
+        #with POST data. This prevents data from being twice if a user hits the Back button
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
